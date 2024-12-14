9ce8f0a34c7c5fa9a6ab75c4d30a80d5086a8254
tushiroda <taeshinushiroda@gmail.com>
2024-12-13 23:13:16 -0500

    we love committing to main (i ran prettier)

--- src/range-slider.js
+++ src/range-slider.js
@@ -1,17 +1,17 @@
 window.addEventListener("load", () => {
-  document.querySelectorAll(".range-slider").forEach(rangeSlider => {
+  document.querySelectorAll(".range-slider").forEach((rangeSlider) => {
     const ranges = rangeSlider.querySelectorAll("input[type=range]");
     const rangeMin = rangeSlider.querySelector(".rangeMin");
     const rangeMax = rangeSlider.querySelector(".rangeMax");
 
-    ranges[0].addEventListener("input", e => {
+    ranges[0].addEventListener("input", (e) => {
       if (+ranges[0].value >= +ranges[1].value) {
         ranges[0].value = +ranges[1].value - 1;
       }
       rangeMin.innerHTML = ranges[0].value;
     });
 
-    ranges[1].addEventListener("input", e => {
+    ranges[1].addEventListener("input", (e) => {
       if (+ranges[1].value <= +ranges[0].value) {
         ranges[1].value = +ranges[0].value + 1;
       }
